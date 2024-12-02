25408856a9a660c76744ac9fd46db0ed049d29ee
Omriivri <136965490+Omriivri@users.noreply.github.com>
2024-12-02 19:23:59 +0200

    Creation of function that create a new comment

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -2,4 +2,6 @@ const express = require('express');
 const router = express.Router();
 const Comment = require('../controllers/comments');
 
+router.post('/', Comment.CreateComment);
+
 module.exports = router;
