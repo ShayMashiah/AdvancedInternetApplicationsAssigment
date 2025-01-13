4c54dfd2f55f6325137c4615402f4ef6c10b62a1
Omriivri <136965490+Omriivri@users.noreply.github.com>
2025-01-13 15:51:11 +0200

    Adding comments swagger

--- src/routes/commentRoutes.ts
+++ src/routes/commentRoutes.ts
@@ -37,13 +37,148 @@ import {authMiddleware} from '../controllers/auth';
  *         
  */
 
+/**
+ * @swagger
+ * /comment:
+ *   get:
+ *     summary: Get all comments
+ *     tags: [Comment]
+ *     responses:
+ *       200:
+ *         description: A list of all comments
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Comment'
+ *       400:
+ *         description: Error getting comments
+ */
+
 router.get('/', Comment.GetAll);
+
+
+/**
+ * @swagger
+ * /comment/{id}:
+ *   get:
+ *     summary: Get a comment by its ID
+ *     tags: [Comment]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: The ID of the comment to be fetched
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: The requested comment
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Comment'
+ *       400:
+ *         description: Error getting comment
+ *       404:
+ *         description: Comment Not found
+ */
 
 router.get('/:id', Comment.GetById);
 
+
+/**
+ * @swagger
+ * /comment:
+ *   post:
+ *     summary: Create a new comment
+ *     tags: [Comment]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Comment'
+ *     responses:
+ *       201:
+ *         description: The created comment
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Comment'
+ *       400:
+ *         description: Bad request 
+ *       401:
+ *         description: Unauthorized 
+ */
+
 router.post('/', authMiddleware, Comment.Create);
 
+/**
+ * @swagger
+ * /comment/{id}:
+ *   put:
+ *     summary: Update an existing comment
+ *     tags: [Comment]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: The ID of the comment to be updated
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Comment'
+ *     responses:
+ *       200:
+ *         description: The updated comment
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Comment'
+ *       400:
+ *         description: Bad request 
+ *       401:
+ *         description: Unauthorized 
+ *       404:
+ *         description: Comment Not found 
+ */
+
 router.put('/:id', authMiddleware,  Comment.Update);
+
+
+/**
+ * @swagger
+ * /comment/{id}:
+ *   delete:
+ *     summary: Delete an existing comment
+ *     tags: [Comment]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: The ID of the comment to be deleted
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Successfully deleted the comment
+ *       401:
+ *         description: Unauthorized
+ *       404:
+ *         description: Comment Not found 
+ */
 
 router.delete('/:id', authMiddleware, Comment.Delete);
 
